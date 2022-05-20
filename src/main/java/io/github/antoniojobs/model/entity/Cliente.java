796fdd4313904f59346ebd45e8ec1226b56be4a0
José Antonio <br014801398740@enel.com>
2022-05-20 18:36:05 -0300

    validacao nome e CPF funcional

--- src/main/java/io/github/antoniojobs/model/entity/Cliente.java
+++ src/main/java/io/github/antoniojobs/model/entity/Cliente.java
@@ -20,11 +20,13 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @Column(name ="nome", nullable = false, length = 150)
+    @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
     @Column(nullable = false,length = 11)
-    @NotNull
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
     @Column(name ="data_cadastro",updatable = false)
