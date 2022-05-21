c29dd82ae7b9cbec721ef3714a1ef811f47476fd
José Antonio <br014801398740@enel.com>
2022-05-21 14:04:04 -0300

    tratamento de excetions de cliente ok

--- src/main/java/io/github/antoniojobs/rest/ClienteController.java
+++ src/main/java/io/github/antoniojobs/rest/ClienteController.java
@@ -31,7 +31,7 @@ public class ClienteController {
     public Cliente acharPorId(@PathVariable Integer id){
         return repository
                 .findById(id)
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
     }
 
     @DeleteMapping("{id}")
@@ -43,12 +43,12 @@ public class ClienteController {
                     repository.delete(cliente);
                     return Void.TYPE;
                 })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar(@PathVariable Integer id,@RequestBody Cliente clienteAtualizado){
+    public void atualizar(@PathVariable Integer id,@RequestBody @Valid Cliente clienteAtualizado){
         repository
                 .findById(id)
                 .map( cliente ->{
@@ -56,7 +56,7 @@ public class ClienteController {
                     cliente.setCpf(clienteAtualizado.getCpf());
                     return repository.save(cliente);
                 })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
     }
 
     @GetMapping("/")
